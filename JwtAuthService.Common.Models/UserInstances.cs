be3fcbf8daa96d27cff4873a13ace131886f5e61
Aldane Barrett <barrett.aldane22@gmail.com>
2023-02-23 14:51:50 -0500

    update user password

--- JwtAuthService.Common.Models/UserInstances.cs
+++ JwtAuthService.Common.Models/UserInstances.cs
@@ -10,7 +10,7 @@ namespace JwtAuthService.Common.Models
 
 		public List<UserModel> Users = new List<UserModel>()
 		{
-			new UserModel() { Username = "coot", EmailAddress = "test_user@tester.com", FullName = "Test User", Password = "test_123", Role = "Admin"},
+			new UserModel() { Username = "coot", EmailAddress = "test_user@tester.com", FullName = "Test User", Password = "tester_123", Role = "Admin"},
 
             new UserModel() { Username = "tommie", EmailAddress = "test_user2@tester.com", FullName = "Test User2", Password = "password124", Role = "contibutor"}
         };
